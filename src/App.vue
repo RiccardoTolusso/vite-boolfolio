b6456b583813fb1ad3d8ac90d80686f81085507d
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-07-31 10:09:05 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      title: 'Progetti',
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .container{
+    h1{
+      color: red;
+    }
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.scss'
+import App from './App.vue'
+import axios from 'axios'
+import * as bootstrap from 'bootstrap'
+
+createApp(App).mount('#app')
